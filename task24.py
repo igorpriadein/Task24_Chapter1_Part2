bf1d27246b9c480e17477b08e9859239cdf7656b
igorpriadein <iga2456@gmail.com>
2020-02-28 18:55:35 +0600

    Task24 Part2

---
+++ task24.py
@@ -0,0 +1,10 @@
+# Напишите функцию которая будет определять полигдром ли введенная строка. Если да 2
+# то печатать “True”, если нет “False”.
+
+string = "нет"
+string2 = string[::-1]
+if string == string2:
+    print("True")
+else:
+    print("False")
+
